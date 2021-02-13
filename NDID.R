e81c2a95b98d5e5cc3cca41c688afa65f54a1cc7
YIBELTAL AREGA <73569566+Yab29@users.noreply.github.com>
2021-02-13 21:32:01 +0800

    Add files via upload

---
+++ NDID.R
@@ -0,0 +1,113 @@
+##g++ version need 4.9 or upper when install rstan package
+##recommend use new version of R
+##if you meet some problem about libxml-2.0.pc when install xml2 package, you need to find out the folder which include libxml-2.0.pc and add the path of this folder in variable "PKG_CONFIG_PATH"
+library(getopt)
+##--------------------------------------------------------------
+spec = matrix(c(
+  'input','i', 1, "character","input file",
+  'prefix','p', 2, "character","output prefix (default \"out\")",
+  'help','h', 0, "logical","print help"
+), byrow=TRUE, ncol=5)
+opt = getopt(spec) 
+
+if (!is.null(opt$help) || is.null(opt$input)) {
+  cat(paste(getopt(spec, usage = T), "\n"))
+  q()
+}
+
+input_file=opt$input
+output_prefix="out"
+if (!is.null(opt$prefix)) {
+  output_prefix=opt$prefix
+}
+
+cat("----------------NDID is running----------------------------\n")
+##----------------------------------------------------
+
+library(fANCOVA)
+library(tidyverse)
+library(qvalue)
+
+##--------------------------------------------------------
+
+yab <- read.delim(input_file) 
+
+yab[yab$selfAvg_1 == 0, "selfAvg_1"] <- 0.5
+yab[yab$selfAvg_2 == 0, "selfAvg_2"] <- 0.5
+
+yab$D = 0.5*log2(yab$selfAvg_2) + 0.5*log2(yab$selfAvg_1)
+outlier = boxplot(yab$D, plot=FALSE)$out
+
+if(length(outlier) != 0) {
+  yab = yab[-which(yab$D %in% outlier),]
+} else {
+  yab = yab
+}
+
+c1 = sum(yab$ipet_1)
+c2 = sum(yab$ipet_2)
+
+if(c1 > c2){
+  yab$ipet_2 = yab$ipet_2 *(c1/c2)
+} else{
+  yab$ipet_1 = yab$ipet_1 *(c2/c1)
+}
+
+d1 = sum(yab$selfAvg_1)
+d2 = sum(yab$selfAvg_2)
+
+if(d1 > d2){
+  yab$selfAvg_2 = yab$selfAvg_2 *(d1/d2)
+} else{
+  yab$selfAvg_1 = yab$selfAvg_1 *(d2/d1)
+}
+
+
+y1 = yab$ipet_1
+y2 = yab$ipet_2
+
+M = log2(y2/y1)
+D = 0.5*log2(yab$selfAvg_2) + 0.5*log2(yab$selfAvg_1)
+
+png(file="Before_Normalization.png", width=500, height=389)
+model1 = loess.as(D,M, plot = TRUE,pch=20)
+abline(h=0, lwd=2, lty=1, col="red")
+dev.off()
+
+log_y1_norm = log2(y1) + (model1$fitted/2)
+log_y2_norm = log2(y2) - (model1$fitted/2)
+
+y1_norm = 2^log_y1_norm
+y2_norm = 2^log_y2_norm
+M_norm = log2(y2_norm/y1_norm) 
+
+png(file="After_Normalization.png", width=500, height=389)
+model2 = loess.as(D, M_norm, plot = TRUE,pch=20)
+dev.off
+
+zi = (M_norm - mean(M_norm))/sd(M_norm)
+
+p.val = 2*pnorm(abs(zi), lower.tail = FALSE)
+fdr = p.adjust(p.val, method = "BH", n = length(p.val))
+
+intensity = c()
+
+for (i in 1:nrow(yab)) {
+  if (M_norm[i] > 0) {
+    intensity[i] = "2"
+  } else 
+    intensity[i] = "1"
+} 
+
+output = data.frame(yab$chrom1, yab$start1,yab$end1, 
+                   yab$chrom2, yab$start2, yab$end2,y1,y2,
+                   y1_norm, y2_norm,p.val, fdr,intensity)
+
+colnames(output) <- c("chrom1","start1", "end1", "chrom2",
+                      "start2", "end2","ipet_1","ipet_2",
+                      "Normalized_Ipet_1","Normalized_Ipet_2",
+                      "P-value","p.adjust", "intensity type")
+
+significant = output %>% filter(output$p.adjust < 0.05)
+
+write.table(significant, paste(output_prefix,"significant_interaction.txt",sep="_"), sep="\t", row.names = F, quote=FALSE)
